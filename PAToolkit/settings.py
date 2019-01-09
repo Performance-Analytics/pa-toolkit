fa11436cdffd19573106a65adf16563359dceabc
Carter Hinsley <kvazau@protonmail.com>
2019-01-09 17:28:44 -0500

    Add pstd to installed apps

--- PAToolkit/settings.py
+++ PAToolkit/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pstd.apps.PstdConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
