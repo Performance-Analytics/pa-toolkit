10d7819b929032b7bf81009f5c3d71ba9d799d90
Carter Hinsley <kvazau@protonmail.com>
2019-01-09 18:39:32 -0500

    PSTD: Add prototype list view and placeholder edit, query, result views

--- pstd/urls.py
+++ pstd/urls.py
@@ -3,5 +3,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.index, name='index'),
+
+    path('training-cycles/',
+         views.training_cycle_list,
+         name="list"),
+
+    path('<int:training_cycle_id>/edit/',
+         views.training_cycle_edit,
+         name="edit"),
+
+    path('<int:training_cycle_id>/query/',
+         views.session_query,
+         name="query"),
+
+    path('<int:training_cycle_id>/result/',
+         views.session_result,
+         name="result"),
+
 ]
--- pstd/views.py
+++ pstd/views.py
@@ -1,7 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import TrainingCycle, TrainingCycleConfig
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at PSTD index.")
+
+def session_query(request, training_cycle_id):
+    return HttpResponse("Querying for a session in training cycle {}.".format(
+        training_cycle_id
+    ))
+
+def session_result(request, training_cycle_id):
+    return HttpResponse("Displaying a session in training cycle {}.".format(
+        training_cycle_id
+    ))
+
+def training_cycle_edit(request, training_cycle_id):
+    return HttpResponse("Editing training cycle {}.".format(training_cycle_id))
+
+def training_cycle_list(request):
+    training_cycles = TrainingCycle.objects.all()
+    
+    template = loader.get_template('pstd/list.html')
+    context = {
+        'training_cycles': training_cycles
+    }
+    return HttpResponse(template.render(context, request))
