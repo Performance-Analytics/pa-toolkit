a1eafb111d99a4559725dff08fe348f811e54fea
Carter Hinsley <kvazau@protonmail.com>
2019-01-09 18:02:26 -0500

    Add pstd models to admin model

--- pstd/admin.py
+++ pstd/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import TrainingCycle, TrainingCycleConfig
+
 # Register your models here.
+
+admin.site.register(TrainingCycle)
+admin.site.register(TrainingCycleConfig)
