a8f0e66e9aa2a323c01cf5e6d9cc88418d2fb277
Carter Hinsley <carterhinsley@outlook.com>
2019-01-10 10:16:12 +0000

    Add horrific xipio workaround for G+ OAuth2 limitation

--- PAToolkit/settings.py
+++ PAToolkit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cv5!5fm^gd8cqek&ocoj-iy7e0p+@)qac*4ylfhygc!^w$7#g+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['206.189.187.122.xip.io']
 
 
 # Application definition
