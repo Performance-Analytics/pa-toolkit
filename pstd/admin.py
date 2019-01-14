5626a868e2016ca437bfb0e58e48079dee09b06f
Carter Hinsley <kvazau@protonmail.com>
2019-01-14 02:38:46 -0500

    Add PresetTrainingCycle to admin model registry

--- pstd/admin.py
+++ pstd/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models.training_cycles import TrainingCycle, TrainingCycleConfig
+from .models.training_cycles import PresetTrainingCycle, TrainingCycle, TrainingCycleConfig
 
 # Register your models here.
 
+admin.site.register(PresetTrainingCycle)
 admin.site.register(TrainingCycle)
 admin.site.register(TrainingCycleConfig)
