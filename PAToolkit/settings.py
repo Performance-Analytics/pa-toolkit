234e8fe598eab40038b8aedaea6996721f0c2fb0
Carter Hinsley <kvazau@protonmail.com>
2019-01-14 16:13:51 -0500

    Remove SECRET_KEY from settings

--- PAToolkit/settings.py
+++ PAToolkit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cv5!5fm^gd8cqek&ocoj-iy7e0p+@)qac*4ylfhygc!^w$7#g+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
