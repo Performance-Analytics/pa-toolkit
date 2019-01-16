c2f4465a70b8c72f743bd69a3cfb2500db5b0f78
Carter Hinsley <kvazau@protonmail.com>
2019-01-16 16:03:35 -0500

    Add site homepage

--- PAToolkit/urls.py
+++ PAToolkit/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.auth.views import LogoutView
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='homepage.html')),
     path('pstd/', include('pstd.urls')),
     path('admin/', admin.site.urls),
     path('', include('social_django.urls', namespace='social')),
